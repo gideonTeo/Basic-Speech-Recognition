227dd1fd8ef4876438130d0d8b0e46001d6365de
Zhi Kai Teo <49636870+gideonTeo@users.noreply.github.com>
2021-02-22 16:37:36 +1100

    Add files via upload

---
+++ audio_recog.py
@@ -0,0 +1,42 @@
+import speech_recognition as sr
+
+mic_name = "MacBook Pro Microphone" # set Mic
+
+# Sample rate is how often values are recorded
+sample_rate = 48000
+
+# use powers of 2 such as 1024 or 2048 (bytes of data) -- Buffer
+chunk_size = 2048
+
+# Initialize the recognizer
+r = sr.Recognizer()
+
+# generate a list of all microphones
+mic_list = sr.Microphone.list_microphone_names()
+
+# the following loop aims to set the device ID of the mic that
+# we specifically want to use to avoid ambiguity.
+for i, microphone_name in enumerate(mic_list):
+    if microphone_name == mic_name:
+        device_id = i
+
+# use the microphone as source for input; specify which device ID to specifically look for
+with sr.Microphone(device_index=device_id, sample_rate=sample_rate, chunk_size=chunk_size) as source:
+
+    # wait for a second to let the recognizer adjust the energy threshold based on the surrounding noise level
+    r.adjust_for_ambient_noise(source)
+    print("Say Something")
+
+    # listens for the user's input
+    audio = r.listen(source)
+
+    try:
+        text = r.recognize_google(audio)
+        print("you said: " + text)
+
+    # raise exception
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
